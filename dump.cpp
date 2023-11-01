4843a154c26d6a1a7f1bf8ef7c65b0026a4426eb
scammerlikely <137663707+scammerlikely@users.noreply.github.com>
2023-10-31 23:22:34 -0400

    Create dump.cpp

---
+++ dump.cpp
@@ -0,0 +1,15 @@
+#include flag 
+#include dump
+#include debug
+
+
+debug.StatusRemain
+debug.LeveLRBHG
+debug.ROBLOXCORPATIONS
+
+dump.ROBLOXCORPATIONS
+dump.FLAG_STATUS
+dump.l33p(wait[0]) 
+
+
+// this will dump ROBLOXCORPATIONS which u can find in local - 
